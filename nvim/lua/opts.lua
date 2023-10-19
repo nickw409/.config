b5dd6b390894bc9b951dc340962d1f0bcaa975d9
nickw409 <nwiley333@gmail.com>
2023-10-18 18:38:46 -0700

    Init

---
+++ nvim/lua/opts.lua
@@ -0,0 +1,26 @@
+local opt = vim.opt
+
+opt.colorcolumn = '80'
+opt.number = true		--show line numbers
+--opt.relativenumber = true
+
+opt.encoding = 'utf8'
+opt.fileencoding = 'utf8'
+
+opt.syntax = 'ON'
+opt.termguicolors = true
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.hlsearch = true
+
+opt.expandtab = true
+opt.shiftwidth = 3
+opt.softtabstop = 3
+opt.tabstop = 3
+
+opt.splitright = true
+opt.splitbelow = true
+
+
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+
+-- nightfox
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme nightfox]])
+		end,
+	}
+}
